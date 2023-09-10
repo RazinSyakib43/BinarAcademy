5fea5bc03fb60e54db53292a37a563269632a4be
Razin Syakib <muhammadrazin14@gmail.com>
2023-09-10 15:46:35 +0700

    update 1092023

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
 // Owlcarousel
-$(document).ready(function(){
+$(document).ready(function () {
   $(".owl-carousel").owlCarousel({
-  	loop:true,
-    margin:10,
-    nav:true,
-	autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true,
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
     center: true,
     navText: [
-	    "<i class='fa fa-angle-left'></i>",
-	    "<i class='fa fa-angle-right'></i>"
-	],
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:3
-        }
-    }
+      "<i class='fa fa-angle-left'></i>",
+      "<i class='fa fa-angle-right'></i>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 3,
+      },
+    },
   });
+});
+
+
+$(document).ready(function () {
+  const owl = $(".owl-carousel");
+  owl.owlCarousel({
+    loop: true,
+    margin: 5,
+    nav: false,
+    items: 1,
+    dots: true,
+  });
+
+  // Custom Nav
+
+  $(".owl-carousel__next").click(() => owl.trigger("next.owl.carousel"));
+
+  $(".owl-carousel__prev").click(() => owl.trigger("prev.owl.carousel"));
 });
